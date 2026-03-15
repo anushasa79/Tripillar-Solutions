093f58691241bae78dd96b2812a6c1f18248d86d
Anusha.S.A <anushasa79@gmail.com>
2026-03-15 08:12:22 +0530

    Update and rename Biggest.java to week1/Biggest.java

--- week1/Biggest.java
+++ week1/Biggest.java
@@ -7,23 +7,14 @@ class Biggest {
 
         return z > (x > y ? x : y) ? z : ((x > y) ? x : y);
     }
-
-   
-    public static void main(String[] args)
+   public static void main(String[] args)
     {
-
-       
-        int a, b, c;
-
-        
-        int largest;
+     int a, b, c;
+   int largest;
         a = 5;
         b = 10;
         c = 3;
-        
-        largest = biggestOfThree(a, b, c);
-
-        
+ largest = biggestOfThree(a, b, c);    
         System.out.println(largest
                            + " is the largest number.");
     }
